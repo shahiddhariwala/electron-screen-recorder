131c1e45322d9c1a758289126733af68841c3d50
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-09-15 12:07:30 +0530

    Screen Recorder without audio done

--- MyScreenRecorder/src/index.js
+++ MyScreenRecorder/src/index.js
@@ -11,6 +11,10 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
   });
 
   // and load the index.html of the app.
@@ -25,10 +29,10 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
